5083f9125cd3f38b6a27c3de38cf186b1b67cdc0
Patrizio Simona <mail@psimona.ch>
2017-04-14 09:53:14 +0200

    First build configuration for linux only

---
+++ travis_build.sh
@@ -0,0 +1,11 @@
+./utilikit/check_requirements.py --common --quilt
+mkdir build/
+mkdir build/sandbox
+mkdir build/downloads
+./utilikit/prepare_sources.py
+./utilikit/substitute_domains.py
+./utilikit/generate_build_files.py debian --flavor conservative --apply-domain-substitution
+# Checks and reports any additional packages needed
+dpkg-checkbuilddeps
+cd build/sandbox
+dpkg-buildpackage -b -uc
